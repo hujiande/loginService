57659b692b63c4f67e9e7b9df342f3c6195a72e3
jdhu <jdhu@easipay.net>
2019-09-06 11:53:48 +0800

    xiugai

--- src/main/java/com/hjd/net/managerservice/config/shiro/ShiroRealm.java
+++ src/main/java/com/hjd/net/managerservice/config/shiro/ShiroRealm.java
@@ -80,7 +80,7 @@ public class ShiroRealm extends AuthorizingRealm {
                 throw new AuthenticationException("Access denied, Token verity error.");
             }
 		}
-        throw new AuthenticationException("Token expired or incorrect.");
+        return new SimpleAuthenticationInfo(token, token, "shiroRealm");
     }
 
 	/**
